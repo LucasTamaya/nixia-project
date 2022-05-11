60c10a7ad95448e4d43246c97ac465139a35cdfb
LucasTamayaGIT <lucastamaya.974@gmail.com>
2022-05-11 14:00:59 +0400

    maj de l'api endpoint

--- server/src/config/mongoDbConnection.js
+++ server/src/config/mongoDbConnection.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 // Connexion à la base de donnée
 const mongoDbConnection = () => {
-  mongoose.connect(process.env.MONGODB_URI);
+  mongoose.connect(process.env.MONGODB_URI + "&w=majority");
   mongoose.connection.once("open", () => console.log("MONGODB connected"));
 };
 
